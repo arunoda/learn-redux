aeb0485a589fbc7cde1bc9128ba8214b04b39204
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-12-20 14:31:38 +0530

    Add client side bindings

--- server/graphql.js
+++ server/graphql.js
@@ -1,9 +1,11 @@
 import Schema from './schema';
 import express from 'express';
 import expressGraphql from 'express-graphql';
+import cors from 'cors';
 
 const port = 4000;
 const app = express();
+app.use(cors({origin: true, credentials: true}));
 app.use('/', expressGraphql({schema: Schema, graphiql: true}));
 
 console.log(`GraphQL Endpoint started on port: ${port}`);
