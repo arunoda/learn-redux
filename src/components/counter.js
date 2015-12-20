e9594638229ad43fce43114d6ffb0495312b58f3
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-12-20 12:32:49 +0530

    Change button style a bit

--- src/components/counter.js
+++ src/components/counter.js
@@ -6,14 +6,23 @@ const ErrorBanner = ({error}) => (
   </div>
 );
 
+const buttonStyle = {
+  width: 30,
+  height: 30,
+  marginRight: 5,
+  backgroundColor: '#fff',
+  border: '1px solid #ccc',
+  outline: 0
+};
+
 const Counter = ({error, count, add, remove}) => (
-  <div>
+  <div style={{fontSize: 30}}>
     <div>
       Count: {count}
     </div>
     <div>
-      <button onClick={remove}>-</button>
-      <button onClick={add}>+</button>
+      <button onClick={remove} style={buttonStyle}>-</button>
+      <button onClick={add} style={buttonStyle}>+</button>
     </div>
     {error? <ErrorBanner error={error} /> : null}
   </div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import Lokka from 'lokka';
+import LokkaTransport from 'lokka-transport-http';
 
 import rootReducer from './reducers';
 import Counter from './containers/counter';
+
+const clinet = new Lokka({
+  transport: new LokkaTransport('http://localhost:4000')
+});
 
 const myStore = applyMiddleware(thunk)(createStore)(rootReducer);
 
@@ -21,3 +27,4 @@ const render = () => {
 
 myStore.subscribe(render);
 render();
+
